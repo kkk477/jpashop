126def3997f3476128578a8e045861fb38b338a6
HoWoong <dlghdnd@gmail.com>
2023-07-10 23:18:09 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화. 섹션 3. 섹션 3. API 개발 고급 - 지연 로딩과 조회 성능 최적화. 주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,11 +6,15 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -22,6 +26,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
 //    @GetMapping("/api/v1/orders")
 //    public List<Order> ordersV1() {
@@ -47,6 +52,35 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return new Result(collect);
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public Result<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset, @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @GetMapping("/api/v4/orders")
+    public Result<OrderQueryDto> ordersV4() {
+        List<OrderQueryDto> orders = orderQueryRepository.findOrderQueryDtos();
+        return new Result(orders);
+    }
+
+    @GetMapping("/api/v5/orders")
+    public Result<OrderQueryDto> ordersV5() {
+        List<OrderQueryDto> orders = orderQueryRepository.findAllByDto_optimization();
+        return new Result(orders);
+    }
+
+    @GetMapping("/api/v6/orders")
+    public Result<OrderFlatDto> ordersV6() {
+        List<OrderFlatDto> orders = orderQueryRepository.findAllByDto_flat();
+        return new Result(orders);
     }
 
     @Getter
