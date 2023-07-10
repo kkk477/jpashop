9fac606db6769031546ad019c2d2ddebd7a1e76c
HoWoong <dlghdnd@gmail.com>
2023-07-10 13:58:31 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화. 섹션 3. 섹션 3. API 개발 고급 - 지연 로딩과 조회 성능 최적화. 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,12 +2,13 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,62 +19,65 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-public class OrderSimpleApiController {
+public class OrderApiController {
 
     private final OrderRepository orderRepository;
-    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
-    @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersV1() {
-        List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        return all;
-    }
+//    @GetMapping("/api/v1/orders")
+//    public List<Order> ordersV1() {
+//        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+//
+//    }
 
-    @GetMapping("/api/v2/simple-orders")
-    public Result ordersV2() {
+    @GetMapping("/api/v2/orders")
+    public Result<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        List<SimpleOrderDto> collect = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
 
         return new Result(collect);
     }
 
-    @GetMapping("/api/v3/simple-orders")
-    public Result ordersV3() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-        List<SimpleOrderDto> collect = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
-                .collect(Collectors.toList());
+    @Getter
+    static class OrderDto {
 
-        return new Result(collect);
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
     }
 
-    @GetMapping("/api/v4/simple-orders")
-    public Result ordersV4() {
-        return new Result(orderSimpleQueryRepository.findOrderDtos());
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count =orderItem.getCount();
+        }
     }
 
     @Data
     @AllArgsConstructor
     static class Result<T> {
         private T data;
-    }
-
-    @Data
-    static class SimpleOrderDto {
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-
-        public SimpleOrderDto(Order order) {
-            orderId = order.getId();
-            name = order.getMember().getName();
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
-        }
     }
 }
